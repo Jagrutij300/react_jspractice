5b787cfa53a482420e999cbc477e438e9f4f5056
jagrutij300 <jagrutij300@gmail.com>
2023-12-12 13:21:01 +0530

    image add

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+//How to create a component
+//functional component
+//First letter should be capital of a component
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=()=> {
+  //JSX => javascript xml(simple language)
+  return(
+    <div>
+      <img src="https://th.bing.com/th/id/R.1Hw91XA9c1CuLKp9PhAt1ujZ963ZsagEBf?rik=A7pfx1xvE53nTw&riu=http%3a%2f%2fcdn.history.com%2fsites%2f2%2f2017%2f03%2fGettyImages-157278376.jpg&ehk=PaAPgSua4cvWS0dh6DNT5wmYIGQ89Mf62uheBG1CLL8%3d&risl=1&pid=ImgRaw&r=0"/>
+      <h1>Hello this is my first component</h1>
     </div>
-  );
+    
+  )
+ 
 }
 
 export default App;
+
